017ee0682894e931c04da7e0f61f2922a7cd99fc
aidanfew <aidanfew@byu.edu>
2026-05-18 14:31:14 -0600

    setting up

---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.Objects;
+
+public class AuthData {
+    private final String authToken;
+    private final String username;
+
+    AuthData(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken(){
+        return authToken;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AuthData authData = (AuthData) o;
+        return Objects.equals(authToken, authData.authToken) && Objects.equals(username, authData.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, username);
+    }
+}
