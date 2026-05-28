ed637fca62fd5b199d2cf3b68ac2895ae1f95d5b
aidanfew <aidanfew@byu.edu>
2026-05-27 18:06:57 -0600

    GamesSQLDAO stuff

--- server/src/main/java/dataaccess/GameSqlDAO.java
+++ server/src/main/java/dataaccess/GameSqlDAO.java
@@ -1,13 +1,19 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
 import io.javalin.http.HttpResponseException;
 import model.GameData;
+import results.ListGamesHelperResult;
 import service.GameService;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class GameSqlDAO {
     private Connection connection;
@@ -34,7 +40,9 @@ public class GameSqlDAO {
             stmt.setString(1, null);
             stmt.setString(2, null);
             stmt.setString(3, gameName);
-            stmt.setString(4, null);
+            var serializer = new Gson();
+            String jsonGame = serializer.toJson(new ChessGame());
+            stmt.setString(4, jsonGame);
             stmt.executeUpdate();
         }
     }
@@ -53,9 +61,43 @@ public class GameSqlDAO {
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setInt(1, gameID);
             ResultSet rs = stmt.executeQuery();
-            return new GameData(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4, ))
+            String chessGame = rs.getString(5);
+            var serializer = new Gson();
+            ChessGame game = serializer.fromJson(chessGame, ChessGame.class);
+            return new GameData(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), game);
         }
     }
+
+    public void replaceGame(Integer gameID, GameData oldData, GameData newData) throws Exception{
+        String sql = "UPDATE games SET game=? WHERE gameID=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, gameID);
+            var serializer = new Gson();
+            String newChessGame = serializer.toJson(newData.game());
+            stmt.setString(5, newChessGame);
+            stmt.executeUpdate(sql);
+        }
+    }
+
+    public Collection<ListGamesHelperResult> createHelperList() {
+        ArrayList<ListGamesHelperResult> list = new ArrayList<>();
+        String sql = "SELECT gameID, whiteUsername, blackUsername, gameName FROM games";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Integer gameID = rs.getInt(1);
+                String whiteUsername = rs.getString(2);
+                String blackUsername = rs.getString(3);
+                String gameName = rs.getString(4);
+                list.add(new ListGamesHelperResult(gameID, whiteUsername, blackUsername, gameName));
+            }
+        }
+        return list;
+    }
+
+
+
+
 
 
     private void configureDatabase() throws HttpResponseException {
