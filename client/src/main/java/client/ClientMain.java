9d4df0f00f1d3d16e3fb7e2494c1c5a2b95bc25f
aidanfew <aidanfew@byu.edu>
2026-06-03 18:34:46 -0600

    final changes

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -14,7 +14,7 @@ public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        String serverUrl = "http://localhost:8080";
+        String serverUrl = "http://localhost:5000";
 
         if (args.length == 1) {
             serverUrl = args[0];
--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -4,7 +4,7 @@ public class ServerMain {
 
     public static void main(String[] args) {
         Server server = new Server();
-        server.run(8080);
+        server.run(5000);
 
         System.out.println("♕ 240 Chess Server");
 
