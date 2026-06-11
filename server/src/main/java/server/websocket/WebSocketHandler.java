8773fb48481ea42cb429e3fdfe17857bf1c7fb57
aidanfew <aidanfew@byu.edu>
2026-06-10 21:08:13 -0300

    Connection working

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,11 +1,14 @@
 package server.websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
+import dataaccess.GameSqlDAO;
 import exception.ResponseException;
 import io.javalin.websocket.*;
 import org.jetbrains.annotations.NotNull;
 import websocket.commands.UserGameCommand;
 import org.eclipse.jetty.websocket.common.WebSocketSession;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.ServerMessage;
 
 import javax.swing.*;
@@ -14,6 +17,7 @@ import java.io.IOException;
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private GameSqlDAO gameSqlDAO = new GameSqlDAO();
 
     @Override
     public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
@@ -31,15 +35,15 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         try {
             UserGameCommand userGameCommand = new Gson().fromJson(wsMessageContext.message(), UserGameCommand.class);
             switch (userGameCommand.getCommandType()) {
-                case CONNECT -> connect((WebSocketSession) wsMessageContext.session);
+                case CONNECT -> connect((WebSocketSession) wsMessageContext.session, gameSqlDAO.getGame(userGameCommand.getGameID()).game());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void connect(WebSocketSession session) throws IOException {
-        ServerMessage serverMessage = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+    private void connect(WebSocketSession session, ChessGame game) throws IOException {
+        ServerMessage serverMessage = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
         connections.broadcastConnect(session, serverMessage);
     }
 }
